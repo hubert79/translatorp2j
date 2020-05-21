b1e8716d57f5fd0feedad1885aee0b1f00eb78f4
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-05-21 10:07:31 +0200

    Początek prac

---
+++ src/main/java/com/JTP/JtpApplication.java
@@ -0,0 +1,13 @@
+package com.JTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JTP/JtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
